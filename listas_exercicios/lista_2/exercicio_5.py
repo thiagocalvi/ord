3154107142ea77b9997bf68191daa5aa3ba3b66a
thiagocalvi <thiagohcalvi@gmail.com>
2024-06-08 23:19:06 -0300

    Lista #1 - Ex 5,6,7

--- listas_exercicios/lista_2/exercicio_5.py
+++ listas_exercicios/lista_2/exercicio_5.py
@@ -9,19 +9,19 @@
 import sys
 
 def main():
-    if len(sys.argv) != 3:
-        print("Use python exercicio_5.py <nome_do_arquivo> <sitema de origem> <sistema de destino>")
+    if len(sys.argv) != 4:
+        print("Use python \"exercicio_5.py <nome_do_arquivo> <sitema de origem> <sistema de destino>\"")
         sys.exit(1)
 
     nomeArq = sys.argv[1]
     try:
         arq = open(nomeArq, 'rb')
-        newArq = open(nomeArq+sys.argv[2], 'wb')
+        newArq = open(nomeArq+"_"+sys.argv[3], 'wb')
         if sys.argv[2].upper() == "WINDOWS" and sys.argv[3].upper() == "LINUX":
-            win_to_linux(arq)
+            win_to_linux(arq, newArq)
 
-        elif sys.argv[3].upper() == "LINUX" and sys.argv[2].upper() == "WINDOWS":
-            linux_to_win(arq)
+        elif sys.argv[2].upper() == "LINUX" and sys.argv[3].upper() == "WINDOWS":
+            linux_to_win(arq, newArq)
 
         arq.close()
         newArq.close()
@@ -32,9 +32,30 @@ def main():
 
 def ler_caracter(nomeArq):
     caracter_binario = nomeArq.read(1) 
-    return caracter_binario.decode()
+    return caracter_binario
+
+def win_to_linux(arq, newArq):
+    caracter = ler_caracter(arq)
+    
+    while caracter:
+        if caracter == b'\r':
+            newArq.write(b'\n')
+        else:
+            newArq.write(caracter)
+        caracter = ler_caracter(arq)
 
 
-def win_to_linux(arq):
+def linux_to_win(arq, newArq):
+    caracter = ler_caracter(arq)
+    
+    while caracter:
+        if caracter == b'\n':
+            newArq.write(b'\r')
+        else:
+            newArq.write(caracter)
+        
+        caracter = ler_caracter(arq)
 
-def linux_to_win(arq):
+if __name__ == "__main__":
+    main()
+
